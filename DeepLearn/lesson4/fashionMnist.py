42bdedfa45c52f746386a90a6bebb4e382afcb4f
Lvwenchao <giswenchao@gmail.com>
2020-07-25 11:41:27 +0800

    可视化
    tensorboard 使用

--- DeepLearn/lesson4/fashionMnist.py
+++ DeepLearn/lesson4/fashionMnist.py
@@ -3,6 +3,7 @@
 import os
 import tensorflow as tf
 import numpy as np
+import datetime
 from tensorflow import keras
 from tensorflow.keras import datasets, layers, Sequential, optimizers
 
@@ -24,7 +25,8 @@ train_db = train_db.map(preprocess).shuffle(10000).batch(batch_size)
 test_db = test_db.map(preprocess).shuffle(10000).batch(batch_size)
 
 db_iter = iter(train_db)
-sample = next(db_iter)
+sample = next(db_iter)[0]
+sample_image = tf.reshape(x_train[:25, :, :], [-1, 28, 28, 1])
 print(sample[0].shape, sample[1].shape)
 
 model = Sequential([layers.Dense(256, activation=tf.nn.relu),
@@ -37,10 +39,16 @@ model.summary()
 print(len(model.trainable_variables))
 optimizer = optimizers.Adam(lr=0.01)
 
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "logs/" + current_time
+summary_writer = tf.summary.create_file_writer(log_dir)
+with summary_writer.as_default():
+    tf.summary.image("Train_sample", sample_image, step=0)
+
 
 def main():
     total_correct, total_num = 0, 0
-    for epoch in range(40):
+    for epoch in range(10):
         for step, (x, y) in enumerate(train_db):
             x = tf.reshape(x, [-1, 28 * 28])
             # [b,]->[b,10]
@@ -57,6 +65,8 @@ def main():
 
             if step % 100 == 0:
                 print(step, "loss:", loss_ce)
+                with summary_writer.as_default():
+                    tf.summary.scalar('train-loss', float(loss_ce), step=epoch)
 
         # model test
         for step, (x, y) in enumerate(test_db):
@@ -72,6 +82,8 @@ def main():
 
         acc = total_correct / total_num
         print(epoch, "accuracy:", acc)
+        with summary_writer.as_default():
+            tf.summary.scalar('test-acc', float(acc), step=epoch)
 
 
 if __name__ == '__main__':
